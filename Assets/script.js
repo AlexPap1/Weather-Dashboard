74d42946e3670db3a67bbd2f86fd483adce908a4
AlexPap <arpappagallo@gmail.com>
2022-04-11 16:55:19 -0400

    made enter button trigger search and trimmed search value for cities with spaces

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,8 @@
 var apiKey = "2831e983c10c1f8b557906c4cc256f77";
-var city = document.getElementById('enter-city'); 
+var city = document.getElementById('enter-city');
 var searchButton = document.getElementById('search-button');
 var weatherNow= document.getElementById('weatherNow');
+var savedData= document.getElementById('savedData')
 
 function weatherData(cityName) {
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
@@ -10,11 +11,32 @@ function weatherData(cityName) {
 };
 
 searchButton.addEventListener('click', function () {
-    const searchTerm = city.value;
+    /*trims spaces for cities with spaces to avoid breaking api url*/
+    const searchTerm = city.value.trim();
     weatherData(searchTerm);
     console.log(searchTerm);
-    /*save input in local storage and display under search history... see quiz challenge*/
+    history();
 });
+
+/*makes enter button trigger search button click*/
+document.getElementById("enter-city")
+    .addEventListener("keyup", function(e) {
+        if (e.keyCode === 13) {
+            document.getElementById("search-button").click();
+        }
+    });
+
+/*save input in local storage and display under search history*/
+function history() {
+    savedData.textContent = (city.value);
+    console.log(savedData.textContent);
+    localStorage.setItem("history", JSON.stringify(savedData));
+
+};
+
+/*save multiple city searches and make them buttons to redirect back*/
+/*adjust spacing for history... display under History Banner*/
+
 
 /* https://api.openweathermap.org/data/2.5/weather?q=Newark&appid=2831e983c10c1f8b557906c4cc256f77 
 
